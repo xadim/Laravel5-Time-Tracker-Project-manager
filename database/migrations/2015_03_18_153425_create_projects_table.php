e392293fc5f67d72f2171765eaded27dc7e9ba99
xadim <xadimjaxate@gmail.com>
2015-03-27 18:02:55 -0400

    database tables
    Migrate the database to run the application

---
+++ database/migrations/2015_03_18_153425_create_projects_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('projects', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('title');
+			$table->text('desc')->nullable();
+			$table->text('tags')->nullable();
+			$table->text('unit')->nullable();
+			$table->integer('credentials_id')->unsigned();
+			$table->foreign('credentials_id')->references('id')->on('credentials');
+			$table->string('slug')->unique;
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('projects');
+	}
+
+}
